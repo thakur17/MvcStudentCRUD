ec22958ed047791439bb837247102a84512a645e
Jayanti Upadhyay <updjayanti@gmail.com>
2024-04-15 10:40:28 +0545

    Add project files.

---
+++ WebApplication1/Migrations/20240415034633_first.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class first : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    RollNo = table.Column<int>(type: "INTEGER", nullable: false),
+                    ClassName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Name Please")]
+        public string Name { get; set; }
+        public int RollNo { get; set; }
+        [Required(ErrorMessage = "Class Name Please")]
+        public string ClassName { get; set; }
+    }
+
+}
